ff17ca9263cea337470e91ad8806b31181955ea5
Sudesh08 <sudeshmahato000@gmail.com>
2025-04-22 22:08:48 +0530

    Learning Basic of Python

---
+++ Programming/Number Syatem/Swaping_Of_Two_Num.py
@@ -0,0 +1,19 @@
+# name=input("Enter your name ")
+# print("Hello "+name)
+
+num1 = int(input("Enter first number "))
+num2 = int(input("Enter second number "))
+
+print("Before swapping")
+print("num1 : ", num1)
+print("num2 : " , num2)
+temp=num1
+num1=num2
+num2=temp
+
+print("After swapping :")
+print("num1 : ", num1)
+print("num2 : " , num2)
+
+
+
